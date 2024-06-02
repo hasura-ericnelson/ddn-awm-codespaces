05cb04bd1ad1b19648e607df25583832717ea8d6
Eric Nelson <122289890+hasura-ericnelson@users.noreply.github.com>
2024-06-02 03:10:56 +0000

    added new function to test connector builds and introspection

--- ddn-jpmc-awm/app/connector/myts/functions.ts
+++ ddn-jpmc-awm/app/connector/myts/functions.ts
@@ -4,3 +4,21 @@
 export function hello(name?: string) {
   return `hello ${name ?? "world"}`;
 }
+
+
+/**
+ * @readonly Formats a number as a percentage
+ */
+export function formatPercentage(num?: number): string {
+  // let pct = `${num}% lol`
+  if (typeof num !== 'number' || isNaN(num)) {
+    return `${num} is not valid input.  Only numbers can be formatted to a percentage`;  // Returns '0.00%' if input is undefined, null, or not a number
+  }
+  
+  const pct = (num / 100).toLocaleString(undefined, {
+    style: 'percent',
+    minimumFractionDigits: 2
+  });
+  
+  return pct;
+}
