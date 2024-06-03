42522a2af4a1e18216cbcb08e55a7097722f4d3f
Eric Nelson <122289890+hasura-ericnelson@users.noreply.github.com>
2024-06-03 02:11:25 +0000

    Relationship between the index and processTransactionsTotal working

--- ddn-jpmc-awm/app/connector/myts/functions.ts
+++ ddn-jpmc-awm/app/connector/myts/functions.ts
@@ -15,10 +15,53 @@ export function formatPercentage(num?: number): string {
     return `${num} is not valid input.  Only numbers can be formatted to a percentage`;  // Returns '0.00%' if input is undefined, null, or not a number
   }
   
-  const pct = (num / 100).toLocaleString(undefined, {
+  const pct = num.toLocaleString(undefined, {
     style: 'percent',
     minimumFractionDigits: 2
   });
   
   return pct;
+}
+
+
+
+/**
+ * @readonly Calculates the total of all transactions' baseGrossAmount
+ */
+export function processTransactionsTotal(transactions: Transaction[]): number {
+  const initialValue = 0;
+  const sumWithInitial = transactions.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.baseGrossAmount,
+    initialValue,
+  );
+  return sumWithInitial;
+}
+
+
+
+
+/**
+ * @readonly Formats a number as a percentage
+ */
+export function processPositions(positions: Position[]): number {
+  return 42
+}
+
+export interface Transaction {
+  baseGrossAmount: number;
+  accountNumber:   string;
+  instrumentCode:  string;
+}
+export interface Position {
+  accountBaseCurrency:          string;
+  accountNumber:                string;
+  baseAdjustedCostAmount:       number;
+  baseExpectedIncomeAmount:     number;
+  baseMarketPrice:              number;
+  baseSettledMarketValueAmount: number;
+  baseTradedMarketValueAmount:  number;
+  companyName:                  {
+    raw: string;
+  }[];
+  instrumentCode:               string;
 }
