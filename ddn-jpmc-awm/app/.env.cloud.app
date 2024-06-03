f14df9a3af6bd3aa1211ba1ac69c24895ecfb316
Eric Nelson <122289890+hasura-ericnelson@users.noreply.github.com>
2024-06-03 14:09:37 +0000

    added portfolioPerformance bizlogic function

--- ddn-jpmc-awm/app/.env.cloud.app
+++ ddn-jpmc-awm/app/.env.cloud.app
@@ -1,4 +1,4 @@
 APP_MYELASTIC_READ_URL="http://34.235.138.126:8080"
 APP_MYELASTIC_WRITE_URL="http://34.235.138.126:8080"
-APP_MYTS_READ_URL="https://service-175498c5-538a-4570-a10a-1fe485ce66dc-xoa32wu4oa-uw.a.run.app"
-APP_MYTS_WRITE_URL="https://service-175498c5-538a-4570-a10a-1fe485ce66dc-xoa32wu4oa-uw.a.run.app"
+APP_MYTS_READ_URL="https://service-2001832c-4260-4036-ba3a-f49e0b2fc014-xoa32wu4oa-uw.a.run.app"
+APP_MYTS_WRITE_URL="https://service-2001832c-4260-4036-ba3a-f49e0b2fc014-xoa32wu4oa-uw.a.run.app"
--- ddn-jpmc-awm/app/connector/myts/functions.ts
+++ ddn-jpmc-awm/app/connector/myts/functions.ts
@@ -1,3 +1,62 @@
+
+/**
+ * @readonly Calculate the overall performance of a client's portfolio by analyzing the gains and losses from their positions
+ */
+export function portfolioPerformance(positions: Position[])  {
+  let totalGainLoss = 0;
+  let totalInvested = 0;
+
+  const numPositions = positions.length;
+  positions.forEach(position => {
+    const gainLoss = position.baseSettledMarketValueAmount - position.baseAdjustedCostAmount;
+    totalGainLoss += gainLoss;
+    totalInvested += position.baseAdjustedCostAmount;
+  });
+
+  const percentageReturn = formatPercentage((totalGainLoss / totalInvested));
+  return { numPositions, totalGainLoss, percentageReturn };
+};
+
+
+
+
+
+/**
+ * @readonly Calculates the total of all transactions' baseGrossAmount
+ */
+export function processTransactionsTotal(transactions: Transaction[]): number {
+  const initialValue = 0;
+  const sumWithInitial = transactions.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.baseGrossAmount,
+    initialValue,
+  );
+  return sumWithInitial;
+}
+
+
+
+
+/**
+ * @readonly Formats a number as a percentage
+ */
+export function processPositions(positions: Position[]): number {
+  return 42
+}
+
+
+
+
+
+
+
+
+
+/** 
+ * 
+ * Helper and testing
+ * 
+ */
+
 /**
  * @readonly Exposes the function as an NDC function (the function should only query data without making modifications)
  */
@@ -25,28 +84,12 @@ export function formatPercentage(num?: number): string {
 
 
 
-/**
- * @readonly Calculates the total of all transactions' baseGrossAmount
- */
-export function processTransactionsTotal(transactions: Transaction[]): number {
-  const initialValue = 0;
-  const sumWithInitial = transactions.reduce(
-    (accumulator, currentValue) => accumulator + currentValue.baseGrossAmount,
-    initialValue,
-  );
-  return sumWithInitial;
-}
 
 
 
 
-/**
- * @readonly Formats a number as a percentage
- */
-export function processPositions(positions: Position[]): number {
-  return 42
-}
 
+// Types
 export interface Transaction {
   baseGrossAmount: number;
   accountNumber:   string;
